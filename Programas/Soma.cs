9a6b30e171a11aa54de46ca25d1cb19e1b2cb54a
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2020-09-12 14:24:22 +0000

    update Image and Soma

---
+++ Programas/Soma.cs
@@ -0,0 +1,12 @@
+using System;
+class ClasseSoma
+{
+    public static void Main()
+    {
+        soma();
+    }
+    void soma()
+    {
+        return Console.WriteLine("Não e Victor");;
+    }
+}
