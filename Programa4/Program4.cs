90e20925261a3271a0279144c49b44c9d9b86cd3
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2021-05-28 21:03:51 +0000

    New updates

---
+++ Programa4/Program4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dotnetcore
+{
+    class Program4
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro valor: ");
+            String a = Console.ReadLine();
+            Console.WriteLine("Digite o segundo valor: ");
+            String b = Console.ReadLine();
+            Console.WriteLine("Soma é " + soma(Convert.ToInt32(a), Convert.ToInt32(b)));
+        }
+        public static int soma(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
--- ProgramasAntigos/olaMundo.cs
+++
@@ -1,8 +0,0 @@
-using System;
-class ClasseExemplo
-{
-    public static void Main()
-    {
-        Console.WriteLine("Olá, Mundo!");
-    }
-}
