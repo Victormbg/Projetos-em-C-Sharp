4e9c83411d5f4ebc71c22a668d1e0e2423e5bc62
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2021-05-28 20:33:56 +0000

    New updates to the project

---
+++ Programa2/Program2.cs
@@ -0,0 +1,9 @@
+﻿using System;
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Victor Manuel");
+        }
+    }
+
---
+++ Programa3/Program3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dotnetcore
+{
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            String nome = "Victorr";
+            if (nome == "Victor")
+            {
+                Console.WriteLine("E o Victor");
+            }
+            else
+            {
+                Console.WriteLine("Não e Victor");
+            }
+            }
+    }
+}
